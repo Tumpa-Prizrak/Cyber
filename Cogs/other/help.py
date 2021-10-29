ea17f17f56ae5414063bc321f2dbe333ceac75ee
Tumpa-Prizrak <igrunok.play@yandex.ru>
2021-10-29 22:29:33 +0300

    Слияние с RouC и Хреновый help
    Спасибо "Егор Брон"
    
    Co-Authored-By: Егор Брон <egorbron@inbox.ru>

--- Cogs/other/help.py
+++ Cogs/other/help.py
@@ -16,27 +16,47 @@ class OtherCommand(commands.Cog):
                 }
 
     @commands.command()
-    async def help(self, ctx):
-        categories = []
-        for i in os.listdir("Cogs"):
-            if i.endswith(".db") or i.endswith(".py") or i == '__pycache__':
-                continue
-            else:
-                categories.append(i)
-
-        emb = discord.Embed(title="Команды:", colour=discord.colour.Colour.green())
-
-        for category in categories:
-            desc = ""
-            for i in os.listdir("Cogs/" + category):
-                if i.startswith("config.") or not i.endswith(".py"):
+    async def help(self, ctx, *, command: str = None):
+        if command == None:
+            categories = []
+            for i in os.listdir("Cogs"):
+                if i.endswith(".db") or i.endswith(".py") or i == '__pycache__':
                     continue
                 else:
-                    desc += f"`{i[:-3]}` "
-            emb.add_field(name=self.rus_categories[category], value=desc, inline=False)
-            # print(f"emb.add_field(name={category}, value={desc}, inline=False)")
+                    categories.append(i)
 
-        await ctx.send(embed=emb)
+            emb = discord.Embed(title="Команды:", colour=discord.colour.Colour.green())
+            emb.set_footer(text="Можешь прописать =help <команда>, чтобы узнать больше про нужную команду :)",
+            icon_url="https://cdn.discordapp.com/attachments/880063679570259969/897478783215476826/0c5aa927105c867558d290d6a1f3f72f.webp")
+
+            for category in categories:
+                desc = ""
+                for i in os.listdir("Cogs/" + category):
+                    if i.startswith("config.") or not i.endswith(".py"):
+                        continue
+                    else:
+                        desc += f"`{i[:-3]}` "
+                emb.add_field(name=self.rus_categories[category], value=desc, inline=False)
+                # print(f"emb.add_field(name={category}, value={desc}, inline=False)")
+
+            await ctx.send(embed=emb)
+        else:
+            try:
+                comm = __import__(f"Cogs.moderation.{command}")
+            except ModuleNotFoundError:
+                try:
+                    comm = __import__(f"Cogs.other.{command}")
+                except ModuleNotFoundError:
+                    try:
+                        comm = __import__(f"Cogs.reactions.{command}")
+                    except ModuleNotFoundError:
+                        await ctx.send("Команда не найдена")
+                        raise ModuleNotFoundError
+
+            emb = discord.Embed(title=f"Команда {command}", colour=discord.colour.Colour.green())
+            emb.add_field(name=comm.__doc__, value="Как-то так :)", inline=False)
+
+            await ctx.send(embed=emb)
 
 
 def setup(client):
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import discord
-from discord.ext import commands
-import sqlite3
-import config as c
+import aeval
+import aiohttp
+import asyncio
+import datetime
 import os
+import random
+import requests
+import sqlite3
+import sys
+import time
+import discord
+from discord.ext import commands, tasks
+import config as c
 
-bot = commands.Bot(command_prefix=c.prefix, intents=discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(c.prefix), intents=discord.Intents.all(), case_insensitive=True)
 bot.remove_command("help")
 # slash = InteractionClient(bot)
 conn = sqlite3.connect("Cogs/mysqldb.db")
 curor = conn.cursor()
 
+# Load Cogs
 categories = []
 for i in os.listdir("Cogs"):
     if i.endswith(".db") or i.endswith(".py"):
@@ -27,6 +36,16 @@ for directory in categories:
             continue
 
 
+for i in os.listdir("Cogs"):
+    try:
+        if i.endswith(".py") and not i.startswith("config."):
+            bot.load_extension(f"Cogs.{file[:-3]}")
+    except discord.ext.commands.errors.NoEntryPointError:
+        print(f"NoEntryPointError at Cog {file[:-3]}")
+    except Exception as e:
+        print(f"Error: \"{e}\" at Cog {file[:-3]}")
+
+
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Streaming(name="!help",
@@ -37,24 +56,55 @@ async def on_ready():
     print("Ready")
 
 
-@bot.command()
-async def code(ctx, *, code: str):
+minify_text = lambda txt: f'{txt[:-900]}...\n# ...и ещё {len(txt.replace(txt[:-900], ""))} символов' if len(txt) >= 1024 else txt
+
+
+@bot.command(aliases=['eval', 'aeval', 'evaulate', 'выполнить', 'exec', 'execute', 'code'])
+async def __eval(ctx, *, content):
     if ctx.author.id not in c.owners:
-        return
-
-    in_code = ""
-
-    for i in str.split(code, "\n"):
-        if i.startswith("```"):
-            continue
-        else:
-            in_code += i
-            in_code += "\n"
-
+        return await ctx.send("Кыш!")
+    code = "\n".join(content.split("\n")[1:])[:-3] if content.startswith("```") and content.endswith("```") else content
+    standard_args = {
+        # Стандартные библиотеки и переменные, которые будут определены в коде. Для удобства.
+        "discord": discord,
+        "commands": commands,
+        "bot": bot,
+        "tasks": tasks,
+        "ctx": ctx,
+        "asyncio": asyncio,
+        "aiohttp": aiohttp,
+        "os": os,
+        'sys': sys,
+        "time": time,
+        "datetime": datetime,
+        "random": random,
+        "requests": requests
+    }
+    start = time.time() # import time, для расчёта времени выполнения
     try:
-        exec(in_code)
+        r = await aeval.aeval(f"""{code}""", standard_args, {}) # выполняем код
+        ended = time.time() - start # рассчитываем конец выполнения
+        print(r)
+        if not code.startswith('#nooutput'):
+            # Если код начинается с #nooutput, то вывода не будет
+            embed = discord.Embed(title="Успешно!", description=f"Выполнено за: {ended}", color=0x99ff99)
+            """
+             Есть нюанс: если входные/выходные данные будут длиннее 1024 символов, то эмбед не отправится, а функция выдаст ошибку.
+             Именно поэтому сверху стоит print(r), а так же есть функция minify_text, которая
+             минифицирует текст для эмбеда во избежание БэдРеквеста (слишком много символов).
+            """
+            embed.add_field(name=f'Входные данные:', value=f'`{minify_text(str(code))}`')
+            embed.add_field(name=f'Выходные данные:', value=f'`{minify_text(str(r))}`', inline=False)
+            await ctx.send(embed=embed)
     except Exception as e:
-        await ctx.send(e)
+        ended = time.time() - start
+        if not code.startswith('#nooutput'):
+            code = minify_text(str(code))
+            embed = discord.Embed(title=f"При выполнении возникла ошибка.\nВремя: {ended}",
+                                  description=f'Ошибка:\n```py\n{e}```', color=0xff0000)
+            embed.add_field(name=f'Входные данные:', value=f'`{minify_text(str(code))}`', inline=False)
+            await ctx.send(embed=embed)
+            raise e
 
 
 bot.run(c.token)
