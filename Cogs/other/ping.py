52b7be3f89e10678e0f2a2b756410698f068bc70
Егор Брон <bataikinegor@yandex.ru>
2021-11-24 15:47:32 +0300

    Stupid rewrite, lol

--- Cogs/other/ping.py
+++ Cogs/other/ping.py
@@ -9,9 +9,7 @@ class OtherCommand(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f"pong! Задержка {round(self.client.latency * 1000)} мс")
+    
 
 
 def setup(client):
